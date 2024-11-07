236ff2ecf869b9e5422b3583c4deda6a20454c8b
AgusSulaiman17 <agus.sulaiman172006@gmail.com>
2024-11-07 09:10:48 +0700

    create : backend

---
+++ backend/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "backend/config"
+    "backend/routes"
+    "backend/middleware"
+    "github.com/labstack/echo/v4"
+)
+
+func main() {
+    config.ConnectDB()
+    e := echo.New()
+    if config.DB == nil {
+        panic("Database connection failed. Please check your configuration.")
+    }
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
+    routes.AuthRoutes(e)
+    routes.UserRoutes(e)
+    routes.GenreRoutes(e)
+    routes.PenulisRoutes(e)
+    routes.BookRoutes(e)
+    routes.PeminjamanRoutes(e)
+
+
+    e.Logger.Fatal(e.Start(":8080"))
+}
